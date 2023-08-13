240bbb7ed376b22cd31a9f44f6daf5d0393a77ac
saqib <saqib29111@gmail.com>
2023-08-13 15:06:32 +0600

    Schema & Types

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
 
 // server setup
 const server = new ApolloServer({
-    // typeDef
+    typeDefs
     // resolvers
 })
 
