70971cfec804cd995cd4b35eda7170358ceadac4
saqib <saqib@gain.media>
2025-01-01 18:39:39 +0600

    update config files

---
+++ graphql-auth-app/src/config/config.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import * as  databaseConfig from './databaseConfig'
+import * as  serverConfig from './serverConfig'
---
+++ graphql-auth-app/src/config/database.config.js
@@ -0,0 +1,5 @@
+export const databaseConfig = {
+  postgress_user: process.env.POSTGRES_USER,
+  postgres_password: process.env.POSTGRES_PASSWORD,
+  postgres_db: process.env.POSTGRES_DB,
+}
